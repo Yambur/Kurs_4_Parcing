c510fc16766ad491e226d10e59dfc3499d79be09
bargest <DIZz92@mail.ru>
2023-11-15 13:59:03 +0300

    aga

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.request.hh import RequestHH
 from src.request.sj import RequestSJ
 
-#hh = RequestHH('python')
-#hh.request()
+hh = RequestHH('python')
+hh.request()
 
-sj = RequestSJ('python')
-sj.request()
+#sj = RequestSJ('python')
+#sj.request()
 
--- src/request/sj.py
+++ src/request/sj.py
@@ -12,7 +12,6 @@ class RequestSJ(WorkWithAbstract):
         self.params = {'keywords': keyword, 'page': page}
 
     def request(self):
-        headers = {
-            'X-Api-App-Id': 'v3.r.137951623.25fdf6a929d7a2b8a7c0f7c14fc329fd96912622.c8c509bdc2d0cc6f3023e1f179ea68bdcc9c05ff'}
+        headers = {'X-Api-App-Id': 'v3.r.137951623.25fdf6a929d7a2b8a7c0f7c14fc329fd96912622.c8c509bdc2d0cc6f3023e1f179ea68bdcc9c05ff'}
         responce = requests.get(self.url, params=self.params, headers=headers)
         return WorkWithJson.save_json(responce.json()['objects'])
