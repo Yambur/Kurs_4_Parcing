790dbf82bf4af74bbf5b368e20f705a2c997d8e1
bargest <DIZz92@mail.ru>
2023-11-17 20:51:05 +0300

    ну дай коммитну, че ты

--- vacancy/vacancy_hh.py
+++ vacancy/vacancy_hh.py
@@ -2,9 +2,10 @@ import json
 
 
 class VacancyHH:
-    def __init__(self, title, city, description, salary_from, salary_to):
+    def __init__(self, title, city, requirements,description, salary_from, salary_to):
         self.title = title
         self.city = city
+        self.requirements = requirements
         self.description = description
         self.salary_from = salary_from
         self.salary_to = salary_to
@@ -12,6 +13,7 @@ class VacancyHH:
     def __repr__(self):
         return (f'Вакансия: {self.title} \n'
                 f'Город: {self.city} \n'
+                f'Знания: {self.requirements} \n'
                 f'Описание: {self.description} \n'
                 f'Зарплата от: {self.salary_from} \n'
                 f'Зарплата до: {self.salary_to} \n'
@@ -23,7 +25,7 @@ with open('../cache_json/cache_hh.json', 'r', encoding='utf-8') as file:
     vacancy_s = []
     for i in vacancy:
         if 'name' in i and 'area' in i and 'snippet' in i and i['salary'] is not None and 'from' in i['salary']:
-            vacancy_s.append(VacancyHH(i['name'], i['area']['name'], i['snippet']['responsibility'], i['salary']['from'],
+            vacancy_s.append(VacancyHH(i['name'], i['area']['name'], i['snippet']['requirement'], i['snippet']['responsibility'], i['salary']['from'],
                                      i['salary']['to']))
         else:
             pass
