26760357257a5d864c623df4588ed1b4e3bd162a
bargest <DIZz92@mail.ru>
2023-11-17 21:01:08 +0300

    переместил удобнее и логичней

--- src/vacancy/vacancy_hh.py
+++ src/vacancy/vacancy_hh.py
@@ -1,10 +1,11 @@
 import json
 
 
-class VacancySJ:
-    def __init__(self, title, city, description, salary_from, salary_to):
+class VacancyHH:
+    def __init__(self, title, city, requirements,description, salary_from, salary_to):
         self.title = title
         self.city = city
+        self.requirements = requirements
         self.description = description
         self.salary_from = salary_from
         self.salary_to = salary_to
@@ -12,17 +13,21 @@ class VacancySJ:
     def __repr__(self):
         return (f'Вакансия: {self.title} \n'
                 f'Город: {self.city} \n'
+                f'Знания: {self.requirements} \n'
                 f'Описание: {self.description} \n'
                 f'Зарплата от: {self.salary_from} \n'
                 f'Зарплата до: {self.salary_to} \n'
                 '')
 
 
-with open('../cache_json/cache_hh.json', 'r', encoding='utf-8') as file:
+with open('../../cache_json/cache_hh.json', 'r', encoding='utf-8') as file:
     vacancy = json.load(file)
     vacancy_s = []
     for i in vacancy:
-        vacancy_s.append(
-            VacancySJ(i['profession'], i['town']['title'], i['candidat'], i['payment_from'], i['payment_to']))
+        if 'name' in i and 'area' in i and 'snippet' in i and i['salary'] is not None and 'from' in i['salary']:
+            vacancy_s.append(VacancyHH(i['name'], i['area']['name'], i['snippet']['requirement'], i['snippet']['responsibility'], i['salary']['from'],
+                                     i['salary']['to']))
+        else:
+            pass
 for vacancy in vacancy_s:
     print(vacancy)
