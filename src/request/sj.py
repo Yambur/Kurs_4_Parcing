10abea40f0b0f2fe7d814c27db1c945484eb7363
bargest <DIZz92@mail.ru>
2023-11-22 17:31:48 +0300

    изменил api_key

--- src/request/sj.py
+++ src/request/sj.py
@@ -1,9 +1,9 @@
+from api_key_sj import api_key
 from config import URL_SJ
 from src.abstract_class_api import WorkWithAbstract
 import requests
 
 from src.data_json.work_with_json import WorkWithJson
-
 
 class RequestSJ(WorkWithAbstract):
     """Инициализируем класс, вводим url, определяем ключевое слово и страницу поиска"""
@@ -14,6 +14,6 @@ class RequestSJ(WorkWithAbstract):
 
     def request(self):
         """Заносим парсинг в json файл"""
-        headers = {'X-Api-App-Id': 'v3.r.137951623.25fdf6a929d7a2b8a7c0f7c14fc329fd96912622.c8c509bdc2d0cc6f3023e1f179ea68bdcc9c05ff'}
+        headers = {'X-Api-App-Id': api_key}
         responce = requests.get(self.url, params=self.params, headers=headers)
         return WorkWithJson.save_json(responce.json()['objects'])
